38394da154c4ad3394bc51aec26b56a8ecc2a541
Martin Kukli <digimees@gmail.com>
2021-04-17 15:29:35 +0300

    Add RecipeView

---
+++ src/components/RecipeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h2>Recipe: {{ name }}</h2>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { RecipeType } from '@type';
+
+export default defineComponent({
+  props: {
+    recipe: Object as PropType<RecipeType>,
+  },
+  setup(props) {
+    const name = props.recipe?.name;
+    const ingredients = props.recipe?.ingredients;
+    const directions = props.recipe?.directions;
+
+    return {
+      name,
+      ingredients,
+      directions,
+    };
+  },
+});
+</script>
