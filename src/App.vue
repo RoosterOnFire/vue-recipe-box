b14f9eef53a8882d7b0c5af2a589b7b5abf54240
Martin Kukli <digimees@gmail.com>
2021-04-17 15:14:22 +0300

    Add RecipeList

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,45 @@
-<template></template>
+<template>
+  <div class="flex flex-col items-center h-screen p-4">
+    <h1 class="">{{ heading }}</h1>
+    <RecipeList :list="recipes" />
+    <div class="container"></div>
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import RecipeList from './components/RecipeList.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: {
+    RecipeList,
+  },
+  setup() {
+    const heading = 'Recipe box';
+
+    const recipes = [
+      {
+        name: 'RecipeName1',
+        ingredients: ['ingredient11'],
+        directions: ['directions11'],
+      },
+      {
+        name: 'RecipeName2',
+        ingredients: ['ingredient21', 'ingredient22'],
+        directions: ['directions21', 'directions22'],
+      },
+      {
+        name: 'RecipeName3',
+        ingredients: ['ingredient31', 'ingredient32', 'ingredient33'],
+        directions: ['directions31', 'directions32', 'directions33'],
+      },
+    ];
+
+    return {
+      heading,
+      recipes,
+    };
+  },
 });
 </script>
---
+++ src/components/RecipeList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="row in list" :key="row.name">{{ row.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, PropType } from 'vue';
+import { RecipeListType } from '@type';
+
+export default defineComponent({
+  props: {
+    list: Array as PropType<RecipeListType>,
+  },
+  setup(props) {
+    return {
+      list: props.list,
+    };
+  },
+});
+</script>
