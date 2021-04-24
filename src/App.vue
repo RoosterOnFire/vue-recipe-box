f3cb69e8fb1b9a67db15d4375d92e04db002d673
Martin Kukli <mrtnkukli@gmail.com>
2021-04-24 12:12:17 +0300

    Updatae App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="flex flex-col items-center max-w-2xl h-screen p-4 mx-auto gap-4 text-2xl">
     <h1 class="font-serif italic text-6xl">Recipe box</h1>
-    <RecipeList
+    <recipe-list
       class="p-2 bg-gray-100 border-2 border-gray-400 rounded shadow-md"
       :recipes="recipes"
       :isEmpty="isRecipeListEmpty"
       @select="updateSelectedRecipe"
       @add="addRecipe"
     />
-    <RecipeView
+    <recipe-view
       class="p-2 bg-gray-100 border-2 border-gray-400 rounded shadow-xl"
       :recipe="selectedRecipe"
       @edit="toggleEdit"
     />
-    <RecipeEdit v-if="isEditOpen" :recipe="selectedRecipe" @updateRecipe="updateRecipe" @close="toggleEdit" />
+    <recipe-edit v-if="isEditOpen" :recipe="selectedRecipe" @updateRecipe="updateRecipe" @close="toggleEdit" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import RecipeList from '@component/RecipeList.vue';
-import RecipeView from '@component/RecipeView.vue';
-import RecipeEdit from '@component/RecipeEdit.vue';
-import useRecipes from '@composable/useRecipes';
+import RecipeView from '@component/RecipeView/RecipeView.vue';
+import RecipeEdit from '@component/RecipeEdit/RecipeEdit.vue';
+import useRecipes from './composables/useRecipes';
 
 export default defineComponent({
   name: 'App',
