7f03ab90be0e926a4068f6f0382d09aee79b1f4e
Martin Kukli <mrtnkukli@gmail.com>
2021-04-24 12:19:53 +0300

    Reorg RecipeView

---
+++ src/components/RecipeView/RecipeViewList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>{{ header }}</h3>
+    <ul class="list-disc list-inside">
+      <li class="pl-4" v-for="(value, index) in items" :key="index">{{ value }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, toRefs } from 'vue';
+
+export default defineComponent({
+  props: {
+    header: {
+      type: String,
+      required: false,
+    },
+    items: {
+      type: Array as PropType<string[]>,
+    },
+  },
+  setup(props) {
+    const { header, items } = toRefs(props);
+
+    return {
+      header,
+      items: items || [],
+    };
+  },
+});
+</script>
---
+++ src/components/RecipeView/RecipeViewName.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex items-center space-x-3">
+    <h2 class="text-3xl">Recipe: {{ name }}</h2>
+    <pencil-alt-icon class="h-8 w-8 cursor-pointer" @click="edit" />
+    <trash-recipe />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs } from 'vue';
+import { PencilAltIcon } from '@heroicons/vue/solid';
+import TrashRecipe from '../Trash/TrashRecipe.vue';
+
+export default defineComponent({
+  components: { TrashRecipe, PencilAltIcon },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props, { emit }) {
+    const { name } = toRefs(props);
+
+    return {
+      name,
+      edit: () => emit('edit'),
+    };
+  },
+});
+</script>
